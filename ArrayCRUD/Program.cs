ec5ef1897696639f53dfe5caaeed68d987ae52aa
gg <ghetigradin@gmail.com>
2020-01-05 06:27:52 +0200

    Add project files.

---
+++ ArrayCRUD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayCRUD
+{
+    public class Program
+    {
+        private const int Index2 = 1;
+        private const int LimitArray = 4;
+
+        static void Main()
+        {
+            var sut = new SortedIntArray();
+            var intArr = new[] { 2, 3, 4 };
+            for (int i = 0; i < LimitArray; i++)
+            {
+                sut.Add(i);
+                Console.WriteLine(sut[i]);
+            }
+
+            sut.Add(Index2);
+            for (int i = 0; i < LimitArray + 1; i++)
+            {
+                Console.WriteLine(sut[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ ArrayCRUD/SortedIntArray.cs
@@ -0,0 +1,91 @@
+﻿namespace ArrayCRUD
+{
+    public class SortedIntArray : IntArray
+    {
+        public SortedIntArray() : base()
+        {
+        }
+
+        public override int this[int index]
+        {
+            get => base[index];
+            set
+                {
+                if (!VerifyIfSortedAtSetElement(index, value))
+                {
+                    return;
+                }
+
+                base[index] = value;
+            }
+        }
+
+        public bool VerifyIfSortedAtAdd(int element)
+        {
+            return element >= base[Count - 1];
+        }
+
+        public override void Add(int element)
+        {
+            base.Insert(FindRightPosition(element), element);
+        }
+
+        public override void Insert(int index, int element)
+        {
+            if (!VerifyIfSortedAtInsertElement(index, element))
+            {
+                return;
+            }
+
+            base.Insert(index, element);
+        }
+
+        private bool VerifyIfSortedAtSetElement(int index, int element)
+        {
+            if ((index == 0 && element <= base[index + 1]) || (index == Count && element >= base[index - 1]))
+            {
+                return true;
+            }
+
+            if (element >= base[index - 1] && element <= base[index + 1])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool VerifyIfSortedAtInsertElement(int index, int element)
+        {
+            if ((index == 0 && element <= base[index + 1]) || (index == Count && element >= base[index - 1]))
+            {
+                return true;
+            }
+
+            if (element >= base[index - 1] && element <= base[index])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private int FindRightPosition(int element)
+        {
+            if (Count == 0)
+            {
+                return Count;
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (element <= base[i])
+                {
+                    return i;
+                }
+            }
+
+            return Count;
+        }
+    }
+}
